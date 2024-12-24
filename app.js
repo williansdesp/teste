a066a126b088a9e030cd4c087aa75f216fe5655d
Willians <47144008+williansdesp@users.noreply.github.com>
2024-12-24 08:28:13 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+let listaGenerica = [];
+console.log(listaGenerica);
+
+let linguagensDeProgramacao = ["javaScript", "C", "C++", "Kotlin", "Python"];
+console.log(linguagensDeProgramacao);
+
+linguagensDeProgramacao.push("Java", "Ruby", "GoLang");
+console.log(linguagensDeProgramacao);
+
+let listaNomes = ["Frederico", "Rafaela","Willians"];
+console.log(listaNomes[0]);
+console.log(listaNomes[1]);
+console.log(listaNomes[listaNomes.length-1]);
